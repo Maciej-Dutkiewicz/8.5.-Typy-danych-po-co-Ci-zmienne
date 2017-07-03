f9c4b0910292a367d833848efa750eea1ae15eda
maciej <m.p.dutkiewicz@gmail.com>
2017-07-03 20:50:45 +0200

    działa w consoli

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var a = prompt('Podaj liczbę'),
+	h = prompt('Podaj liczbę'),
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
